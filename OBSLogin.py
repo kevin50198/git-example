8d190d25b62d492cda8525ba702b73c6db21d03d
kevin50198 <47200120+kevin50198@users.noreply.github.com>
2020-03-12 18:01:32 +0800

    time optimization

--- OBSLogin.py
+++ OBSLogin.py
@@ -13,7 +13,7 @@ driver.get("https://ku112.net/")
 driver.maximize_window()
 
 #找到輸入框->輸入內容
-element = driver.find_element_by_id("loginbutton").click()
+driver.find_element_by_id("loginbutton").click()
 
 element = driver.find_element_by_id("accountId")
 element.send_keys("F62127")
@@ -21,5 +21,28 @@ element.send_keys("F62127")
 element = driver.find_element_by_name("accountPwd")
 element.send_keys("qaz123")
 
-element = driver.find_element_by_id("signin").click()
+driver.find_element_by_id("signin")
+time.sleep(5)
 
+def tryclick(count=0): ##保護機制，以防無法定位到還沒渲染出來的元素
+    try:
+        element = driver.find_element_by_id("signin")
+        # elem = driver.find_element_by_xpath(Xpath)  # 如果你想透過Xpath定位元素
+        element.click() # 點擊定位到的元素
+    except:
+        time.sleep(2)
+        count+=1
+        if(count <2):
+            tryclick(count)
+        else:
+            print("cannot locate element")
+tryclick()
+
+#time.sleep(3) # 等待javascript渲染出來，當然這個部分還有更進階的作法，關鍵字是implicit wait, explicit wait，有興趣可以自己去找
+#html = driver.page_source # 取得html文字
+#driver.close()  # 關掉Driver打開的瀏覽器
+#print(html)
+
+#driver.find_element_by_xpath("//*[@id='menu_personal_maintain_bblive']/a").click()
+#time.sleep(1)
+#driver.find_element_by_xpath("//*[@id='FastTransfer']/form/div[2]/div/div/input[1]").click()
