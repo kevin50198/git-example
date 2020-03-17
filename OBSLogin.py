d17e59321b0f13d89d2812c4719a557d618491a5
kevin50198 <47200120+kevin50198@users.noreply.github.com>
2020-03-17 18:33:30 +0800

    transfer and livecustmer

--- OBSLogin.py
+++ OBSLogin.py
@@ -5,12 +5,19 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import re , os , time ,random ,string
+import re , os , time ,random ,string ,logging
 
 #打開網頁,最大化瀏覽器
 driver = webdriver.Chrome()
 driver.get("https://ku112.net/")
 driver.maximize_window()
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
+logger = logging.getLogger(__name__)
+ 
+#logger.debug('Debugging')
+#logger.warning('Warning exists')
+#logger.info('Finish')
 
 #找到輸入框->輸入內容
 driver.find_element_by_id("loginbutton").click()
@@ -21,28 +28,99 @@ element.send_keys("F62127")
 element = driver.find_element_by_name("accountPwd")
 element.send_keys("qaz123")
 
-driver.find_element_by_id("signin")
-time.sleep(5)
+logger.info('Before Login Time') #紀錄登錄前的時間
+driver.find_element_by_id("signin").click()
 
 def tryclick(count=0): ##保護機制，以防無法定位到還沒渲染出來的元素
     try:
-        element = driver.find_element_by_id("signin")
-        # elem = driver.find_element_by_xpath(Xpath)  # 如果你想透過Xpath定位元素
-        element.click() # 點擊定位到的元素
+        driver.find_element_by_xpath("/html/body/div[1]/div/div[3]/div[1]").click() #先選框
+        driver.find_element_by_xpath("/html/body/div[1]/div/div[3]/div[1]/ul/li[5]/a/span").click() #再點擊
+        logger.info('After Login Time / Open PlatfromTransfer Time') #紀錄點擊平台轉帳時間
     except:
-        time.sleep(2)
-        count+=1
+        time.sleep(0.5)
+        count = count+0.5
         if(count <2):
             tryclick(count)
         else:
-            print("cannot locate element")
+            print("cannot locate PlatfromTransfer")
+
+#首頁點擊平台轉帳
 tryclick()
 
-#time.sleep(3) # 等待javascript渲染出來，當然這個部分還有更進階的作法，關鍵字是implicit wait, explicit wait，有興趣可以自己去找
-#html = driver.page_source # 取得html文字
-#driver.close()  # 關掉Driver打開的瀏覽器
-#print(html)
+#找到平台轉帳視窗,並切換過去
+for winHandle in driver.window_handles:
+	driver.switch_to.window(winHandle)
 
-#driver.find_element_by_xpath("//*[@id='menu_personal_maintain_bblive']/a").click()
-#time.sleep(1)
-#driver.find_element_by_xpath("//*[@id='FastTransfer']/form/div[2]/div/div/input[1]").click()
+def tryclick1(count=0): ##保護機制，以防無法定位到還沒渲染出來的元素
+    try:
+        transferout = Select(driver.find_element_by_xpath("//*[@id='tbPlatformTransfer']/tbody/tr/td[2]/div/ul/li[1]/select")) #選擇轉出帳戶輸入框
+        try:
+            transferout.select_by_index(1) #選擇輸入框第二項(以0開始計算)
+        except:
+            time.sleep(0.5)
+            count = count+0.5
+            if(count <2):
+                tryclick(count)
+            else:
+                print("cannot locate index")
+    except:
+        time.sleep(0.5)
+        count = count+0.5
+        if(count <2):
+            tryclick(count)
+        else:
+            print("cannot locate transferout")
+
+def tryclick2(count=0): ##保護機制，以防無法定位到還沒渲染出來的元素
+    try:
+        transferin = Select(driver.find_element_by_xpath("//*[@id='inAccount']")) #選擇轉入帳戶輸入框
+        try:
+            transferin.select_by_index(1) #選擇輸入框第二項(以0開始計算)
+        except:
+            time.sleep(0.5)
+            count = count+0.5
+            if(count <2):
+                tryclick(count)
+            else:
+                print("cannot locate transferin")
+    except:
+        time.sleep(0.5)
+        count = count+0.5
+        if(count <2):
+            tryclick(count)
+        else:
+            print("cannot locate transferout")
+tryclick1()
+tryclick2()
+logger.info('Close PlatfromTransfer Time / OpenLiveService Time') #紀錄選擇轉出帳戶/轉入帳戶完的時間
+driver.close()
+
+#找到原視窗,並切換過去
+for parentHandle in driver.window_handles:
+	driver.switch_to.window(parentHandle)
+
+driver.find_element_by_xpath("/html/body/div[9]/div[1]/ul/li[1]/div").click() #點擊在線客服
+
+#找到在線客服視窗,並切換過去
+for winHandle in driver.window_handles:
+	driver.switch_to.window(winHandle)
+
+def tryclick1(count=0): ##保護機制，以防無法定位到還沒渲染出來的元素
+    try:
+        transferout = Select(driver.find_element_by_xpath("//*[@id='tbPlatformTransfer']/tbody/tr/td[2]/div/ul/li[1]/select")) #選擇轉出帳戶輸入框
+        try:
+            transferout.select_by_index(1) #選擇輸入框第二項(以0開始計算)
+        except:
+            time.sleep(0.5)
+            count = count+0.5
+            if(count <2):
+                tryclick(count)
+            else:
+                print("cannot locate index")
+    except:
+        time.sleep(0.5)
+        count = count+0.5
+        if(count <2):
+            tryclick(count)
+        else:
+            print("cannot locate transferout")
