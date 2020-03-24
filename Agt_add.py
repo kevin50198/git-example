7b456c295617822ad38b7eebd9030d41d70619c3
kevin50198 <47200120+kevin50198@users.noreply.github.com>
2020-03-24 13:39:51 +0800

    add agt in 518

--- Agt_add.py
+++ Agt_add.py
@@ -1,6 +1,5 @@
 # coding:utf-8
 from selenium import webdriver
-from PIL import Image , ImageEnhance
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
